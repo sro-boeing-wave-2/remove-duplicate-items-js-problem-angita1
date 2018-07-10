73e97e93a44f9ae63acd515999e6ebc6f80718f2
Yaiphabee <ksh.yaipha@gmail.com>
2018-07-11 00:23:34 +0530

    modified index.js file of remove-duplicate

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
-
+const removeDuplicateItems = (items) => {
+  const uniqueArray = Array.from(new Set(items));
+  return uniqueArray;
+};
 module.exports = removeDuplicateItems;
